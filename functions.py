d4cc62cff5046f3ca368910292f941028969a63a
flyingmat <paramattimatteo@gmail.com>
2018-06-28 00:51:25 +0100

    Added binomial multiplication + bugfixes.

--- functions.py
+++ functions.py
@@ -1,3 +1,5 @@
+from math import floor
+
 remove_spaces = lambda inlst: [i for i in inlst if i != ' ']
 
 def fix_signs(inlst):
@@ -25,7 +27,7 @@ def get_coefficient(inlst, i):
     while k >= 0 and inlst[k] in '1234567890.':    # keep going backwards to get the full coefficient
         coeff = inlst[k] + coeff
         k -= 1
-    coeff = 1 if not coeff else coeff    # if no coefficient is specified, 1 is assigned
+    coeff = '1' if not coeff else coeff    # if no coefficient is specified, 1 is assigned
     if k >= 0 and inlst[k] == '-':    # check for a minus sign
         coeff = '-' + coeff
     k = 0 if k < 0 else k    # value correction for convert()
@@ -77,6 +79,8 @@ def solve_x0_terms(inlst):
 def divide_func(exps, div):    # uses polynomial long division
     newexps = {}
     for current_exp in range(max(exps)-max(div), -1, -1):
+        if max(exps) - max(div) != current_exp:    # bugfix: FOR Loop coud be changed to something more efficient (needs testing with high exponents)
+            continue
         newexps[current_exp] = exps[max(exps)] / div[max(div)]
         for exp, coeff in div.items():
             m_coeff = exp + current_exp
@@ -85,7 +89,24 @@ def divide_func(exps, div):    # uses polynomial long division
             exps[m_coeff] -= (newexps[current_exp] * coeff)
             if exps[m_coeff] == 0:
                 del exps[m_coeff]    # deletion required because of max() in the main loop that could return a coeff with value 0
-    return newexps if not exps else {}    # if there is a reminder, return an empty dict; could be changed to return reminder
+    return newexps if not exps or not exps[0] else {}    # if there is a reminder, return an empty dict; could be changed to return reminder
+
+def n_factors(n):
+    if type(n) == float and not n.is_integer():
+        yield (n, 1)
+        raise StopIteration
+    else:
+        n = int(n)
+        yield (n, 1)
+    if n % 2 == 0:
+        for i in range(floor(abs(n/2)), 0, -1):
+            if n % i == 0:
+                yield (i, int(n/i))
+    else:
+        tn = floor(abs(n/2))
+        for i in range( (tn - 1 if tn % 2 == 0 else tn), 0, -2 ):
+            if n % i == 0:
+                yield (i, int(n/i))
 
 def x2terms(exps):
     a = exps[2] if 2 in exps else 0
@@ -104,33 +125,50 @@ def pow_diff(poly):
         root1, root2 = int(root1), int(root2)
         if max(poly) % 2 == 0:
             if poly[0] < 0:
-                out = (( { int(max(poly)/2):root1, 0:root2 }, 1 ), ( { int(max(poly)/2):root1, 0:-root2 }, 1 ))
+                xm, x0 = poly[max(poly)]**(1.0/2), abs(poly[0])**(1.0/2)
+                out = (( { int(max(poly)/2):xm, 0:x0 }, 1 ), ( { int(max(poly)/2):xm, 0:-x0 }, 1 ))
         else:
             if poly[0] < 0:
-                out = ( { 1:root1, 0:-root2}, 1 )
+                out = [( { 1:root1, 0:-root2}, 1 )]
             else:
-                out = ( { 1:root1, 0:root2 }, 1 )
+                out = [( { 1:root1, 0:root2 }, 1 )]
+    return out
+
+def binomial_mult(poly, expsort):
+    out = ()
+    for x0t1, x0t2 in n_factors(poly[0]):
+        for xmt1, xmt2 in n_factors(poly[expsort[0]]):
+            if (xmt1*x0t2)+(xmt2*x0t1) == poly[expsort[1]]:
+                p_div1 = { expsort[1]:xmt1, 0:x0t1 }
+                p_div2 = { expsort[1]:xmt2, 0:x0t2 }
+                out = (( p_div1, 1 ), ( p_div2, 1 ))
     return out
 
 def factorize(poly_list, func):
     poly = poly_list.pop(0)
     tmexp = max(poly)
-    if poly[0] == 0:
-        poly = divide_func(poly, { min(poly):1, 0:0 })
-        func.add({ 1:1, 0:0 }, min(poly))
+    div_polys = []
+    if tmexp and poly[0] == 0:
+        func.add({ 1:1, 0:0 }, min([e if e > 0 else tmexp for e in poly]))
+        poly = divide_func(poly, { min([e if e > 0 else tmexp for e in poly]):1 })
         poly_list.append(poly)
-    elif len(poly) == 2 and poly[0]:
+    elif len(poly) == 2 and poly[0]:    # x^2 - 1 -> (x + 1)(x - 1), x^3 - 1, x^3 + 1, etc.
         div_polys = pow_diff(poly)
-        if div_polys:
-            for p, e in div_polys:
-                for div_i in range(e):
-                    poly = divide_func(poly, p)
-                func.add(p, e)
-            poly_list.append(poly)
+    elif len(poly) == 3 and poly[0]:    # x^2 + 2x + 1 -> (x + 1)^2, 3x^2 + 7x + 2 -> (3x + 1)(x + 2), etc. max exp can be > 2
+        expsort = sorted(poly)[::-1]
+        if expsort[0] % 2 == 0 and expsort[0]-expsort[1] == expsort[1]-expsort[2]:
+            div_polys = binomial_mult(poly, expsort)
+    for p, e in div_polys:
+        for div_i in range(e):
+            poly = divide_func(poly, p)
+            if max(p) > 2 or (max(p) == 2 and p[0] and delta_calc(*x2terms(p)) >= 0):
+                poly_list.append(p)
+            else:
+                func.add(p, 1)
     #elif: other polynomials
     if poly_list:
         factorize(poly_list, func)
-    else:
+    elif poly != {0:1}:
         func.add(poly, 1)
 
 class frozendict_exps:
